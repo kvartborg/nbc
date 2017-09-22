e010886a9af9ccfef984f3d287915d187bf1878d
kvartborg <hello@kvartborg.net>
2017-09-22 14:53:26 +0200

    Simplified compile step

--- bin/nbcompile.sh
+++ bin/nbcompile.sh
@@ -10,7 +10,7 @@ fi
 properties="./nbproject/project.properties"
 buildDirProperty=$(cat $properties | grep "build.dir=")
 mainClassProperty=$(cat $properties | grep "main.class=")
-buildDir="./${buildDirProperty/build.dir=/}/classes"
+buildDir="${buildDirProperty/build.dir=/}/classes"
 mainClass="${mainClassProperty/main.class=/}"
 
 # Compile source
@@ -19,22 +19,15 @@ if [ ! -f "./src/${mainClass}.java" ]; then
   exit 1
 fi
 
-cd ./src
-javac "${mainClass}.java"
-cd ..
-
-# Move compiled files to build Directory
+# Create the build directory if it doesn't exist
 if [ ! -d "$buildDir" ]; then
   mkdir -p $buildDir
 fi
 
-cp -r ./src/* $buildDir
-
-# Clean up
-rm $buildDir/*.java
-rm $buildDir/**/*.java 2> /dev/null
-rm ./src/*.class
-rm ./src/**/*.class 2> /dev/null
+# Compile files
+cd ./src
+javac -d "../${buildDir}" "${mainClass}.java"
+cd ..
 
 # Execute the compiled files
 cd $buildDir && java $mainClass
